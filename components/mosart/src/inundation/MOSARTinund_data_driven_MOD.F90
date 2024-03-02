97a7563401f6d5d17927871edcdeea7719d91845
hydrotian <tian.zhou@pnnl.gov>
2024-03-02 13:25:50 -0800

    update Wf calculation

--- components/mosart/src/inundation/MOSARTinund_data_driven_MOD.F90
+++ components/mosart/src/inundation/MOSARTinund_data_driven_MOD.F90
@@ -38,8 +38,8 @@ MODULE MOSARTinund_data_driven_MOD
         if (TRunoff%ff_fp(iunit) > 0._r8) then
             call calculate_volume_exchange(TRunoff%wr(iunit,1),TRunoff%wf_ini(iunit), &
                                            TUnit%wr_bf(iunit),TRunoff%ff_unit(iunit), &
-                                           TRunoff%ff_fp(iunit))
-            TRunoff%yr(iunit,1) = TRunoff%wr(iunit,1) / TUnit%rwidth(iunit) / TUnit%rlen(iunit)
+                                           TRunoff%ff_fp(iunit),TUnit%linear_vcri(iunit))
+            TRunoff%yr(iunit,1) = TRunoff%wr(iunit,1) / TUnit%rwidth(iunit) / TUnit%rlen(iunit) ! water depth is updated here
         endif
 
         TRunoff%ff_ini(iunit)     = TRunoff%ff_fp(iunit)
@@ -70,23 +70,37 @@ MODULE MOSARTinund_data_driven_MOD
 
     end function calculate_flooded_fraction
 
-    subroutine calculate_volume_exchange(wr,wf,wr_bf,ff_unit,ff_fp)
+    subroutine calculate_volume_exchange(wr,wf,wr_bf,ff_unit,ff_fp,Vcri)
+        ! TZ: we should allow the wf > 0 when Vcri< w_total < wr_bf, but we shouldn't limit the max wr to be Vcri.
+        ! Max wr should still be limited by the wr_bf. I suggest adding a calibratable parameter c (ranging 0-1), where wf = (w_total-Vcri)*c
+        ! After we get wf, wr = w_total - wf, then yr will be updated based on the new wr and wr can be greater than Vcri.
 
         implicit none
         real(r8), intent(inout) :: wr, wf
         real(r8), intent(in)    :: wr_bf, ff_unit, ff_fp
         real(r8)                :: w_over     ! channel storage + floodplain storage - channel storage capacity (m^3).
         real(r8)                :: hbf_excess ! bankfull excess water depth
+        real(r8), intent(in)    :: Vcri   ! critical volume, FF = 0 when Vtotal <= Vcri
+        real(r8)                :: c ! longitudinal volume correction coef. 
+
         character( len = * ), parameter :: subname = '(calculate_volume_exchange)'
 
-        w_over  = wr + wf - wr_bf
-        if (w_over > 0) then
+        c = 1.0_r8 ! this can be calibrated in the future to represent the channel longitudinal volume distribution for each unit (e.g. w_over is a part of Vtotal - Vcri)
+
+        if (wr + wf > wr_bf) then
+            w_over = wr + wf - wr_bf
             hbf_excess = w_over / ff_unit
             wr         = wr_bf + hbf_excess * (ff_unit - ff_fp)
             wf         = hbf_excess * ff_fp
         else
-            wr = wr + wf
-            wf = 0._r8
+            w_over  = (wr + wf - Vcri) * c
+            if (w_over > 0) then
+                wr = wr + wf - w_over
+                wf = w_over
+            else
+                wr = wr + wf
+                wf = 0._r8
+            endif
         endif
 
     end subroutine
