6962df3a1df4fad9433e58618628a4ae081d20e1
hydrotian <tian.zhou@pnnl.gov>
2024-03-06 13:08:52 -0800

    update the inundation scheme

--- components/mosart/src/inundation/MOSARTinund_data_driven_MOD.F90
+++ components/mosart/src/inundation/MOSARTinund_data_driven_MOD.F90
@@ -29,7 +29,7 @@ MODULE MOSARTinund_data_driven_MOD
                 Vcri = TUnit%wr_bf(iunit)
             endif
             Vtotal = TRunoff%wr(iunit,1) + TRunoff%wf_ini(iunit)
-            TRunoff%ff_unit(iunit) = calculate_flooded_fraction(Vtotal,TUnit%linear_a(iunit),TUnit%linear_b(iunit))
+            TRunoff%ff_unit(iunit) = calculate_flooded_fraction(Vtotal,TUnit%linear_a(iunit),TUnit%linear_b(iunit),TUnit%a_chnl(iunit))
             if (TRunoff%ff_unit(iunit) > TUnit%a_chnl(iunit) ) then
                 TRunoff%ff_fp(iunit) = TRunoff%ff_unit(iunit) - TUnit%a_chnl(iunit) 
             else
@@ -38,12 +38,13 @@ MODULE MOSARTinund_data_driven_MOD
         else
             TRunoff%ff_fp(iunit) = 0._r8
             TRunoff%ff_fp(iunit) = 0._r8
+            Vcri = TUnit%wr_bf(iunit)
         endif
 
         if (TRunoff%ff_fp(iunit) > 0._r8) then
             call calculate_volume_exchange(TRunoff%wr(iunit,1),TRunoff%wf_ini(iunit), &
                                            TUnit%wr_bf(iunit),TRunoff%ff_unit(iunit), &
-                                           TRunoff%ff_fp(iunit),Vcri)
+                                           TRunoff%ff_fp(iunit),TUnit%a_chnl(iunit), Vcri)
             TRunoff%yr(iunit,1) = TRunoff%wr(iunit,1) / TUnit%rwidth(iunit) / TUnit%rlen(iunit) ! water depth is updated here
         endif
 
@@ -52,11 +53,11 @@ MODULE MOSARTinund_data_driven_MOD
 
     end subroutine inundation_run
 
-    real (r8) function calculate_flooded_fraction(Vtotal,a,b)
+    real (r8) function calculate_flooded_fraction(Vtotal,a,b,f_channel)
 
         implicit none
         real(r8), intent(in) :: Vtotal ! total volume [m^{3}], Vchannel + Vfloodplain
-        real(r8), intent(in) :: a, b   ! FF = a * log(Vtotal) + b
+        real(r8), intent(in) :: a, b, f_channel   ! FF = a * log(Vtotal) + b
         character( len = * ), parameter :: subname = '(calculate_flooded_fraction)'
 
         if (Vtotal < 1._r8) then
@@ -65,8 +66,8 @@ MODULE MOSARTinund_data_driven_MOD
             calculate_flooded_fraction = a*log(Vtotal) + b
             if (calculate_flooded_fraction > 1._r8) then
                 calculate_flooded_fraction= 1._r8
-            elseif (calculate_flooded_fraction < 0._r8) then
-                calculate_flooded_fraction = 0._r8
+            elseif (calculate_flooded_fraction < f_channel) then
+                calculate_flooded_fraction = f_channel ! minimum flooded fraction of a gridcell should be the channel fraction, not zero.
             endif
         endif
 
@@ -74,33 +75,28 @@ MODULE MOSARTinund_data_driven_MOD
 
     end function calculate_flooded_fraction
 
-    subroutine calculate_volume_exchange(wr,wf,wr_bf,ff_unit,ff_fp,Vcri)
-        ! TZ: we should allow the wf > 0 when Vcri< w_total < wr_bf, but we shouldn't limit the max wr to be Vcri.
-        ! Max wr should still be limited by the wr_bf. I suggest adding a calibratable parameter c (ranging 0-1), where wf = (w_total-Vcri)*c
-        ! After we get wf, wr = w_total - wf, then yr will be updated based on the new wr and wr can be greater than Vcri.
+    subroutine calculate_volume_exchange(wr,wf,wr_bf,ff_unit,ff_fp,f_channel,Vcri)
 
         implicit none
         real(r8), intent(inout) :: wr, wf
-        real(r8), intent(in)    :: wr_bf, ff_unit, ff_fp
+        real(r8), intent(in)    :: wr_bf, ff_unit, ff_fp, f_channel
         real(r8)                :: w_over     ! channel storage + floodplain storage - channel storage capacity (m^3).
         real(r8)                :: hbf_excess ! bankfull excess water depth
         real(r8), intent(in)    :: Vcri   ! critical volume, FF = 0 when Vtotal <= Vcri
-        real(r8)                :: c ! longitudinal volume correction coef. 
 
         character( len = * ), parameter :: subname = '(calculate_volume_exchange)'
-
-        c = 1.0_r8 ! this can be calibrated in the future to represent the channel longitudinal volume distribution for each unit (e.g. w_over is a part of Vtotal - Vcri)
 
         if (wr + wf > Vcri) then
             w_over = wr + wf - Vcri
             hbf_excess = w_over / ff_unit
-            wr         = Vcri + hbf_excess * (ff_unit - ff_fp)
+            wr         = Vcri + hbf_excess * (ff_unit - ff_fp) ! It's possible that ff_fp = 0 from `calculate_flooded_fraction`, so w_over will stay in the channel
             wf         = hbf_excess * ff_fp
         else
-                wr = wr + wf
-                wf = 0._r8
+            wr = wr + wf
+            wf = 0._r8
+            ff_fp = 0._r8 ! It's also possible that ff_fp > 0 from `calculate_flooded_fraction`, so we make them consistent
+            ff_unit = f_channel
         endif
-
 
     end subroutine
 
